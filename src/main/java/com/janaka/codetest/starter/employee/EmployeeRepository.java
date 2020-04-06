0806f8d262a9e3e6228e4ba87840fc8600493a23
niroshanjnd <5849822+niroshanjnd@users.noreply.github.com>
2020-04-06 13:21:29 +1000

    Update EmployeeRepository.java
    added spring annotation

--- src/main/java/com/janaka/codetest/starter/employee/EmployeeRepository.java
+++ src/main/java/com/janaka/codetest/starter/employee/EmployeeRepository.java
@@ -2,6 +2,7 @@ package com.janaka.codetest.starter.employee;
 
 import org.springframework.data.repository.CrudRepository;
 
+@Repository
 public interface EmployeeRepository extends CrudRepository<Employee, String>{
 
 }
